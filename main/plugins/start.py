ae63302f27a8906ae4f0f833f1987b2ef7a90852
hemendrasahu67 <57944144+hemendrasahu67@users.noreply.github.com>
2024-04-10 09:42:10 +0530

    Update start.py

--- main/plugins/start.py
+++ main/plugins/start.py
@@ -52,7 +52,7 @@ async def start_command(event):
     buttons = [
         [Button.inline("SET THUMB", data="set"),
          Button.inline("REM THUMB", data="rem")],
-        [Button.url("Join Channel", url="https://t.me/hemubot148")]
+        [Button.url("Join Channel", url="https://t.me/+SKbLY16Sq402MGFl")]
     ]
 
     # Sending photo with caption and buttons
